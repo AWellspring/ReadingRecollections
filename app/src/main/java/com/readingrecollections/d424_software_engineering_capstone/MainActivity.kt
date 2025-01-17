5b1665d7fadda0472c7180c1536d964368450420
ncover7 <ncover7@wgu.edu>
2025-01-16 19:30:41 -0600

    Updated the project to be an Android project.

---
+++ app/src/main/java/com/readingrecollections/d424_software_engineering_capstone/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.readingrecollections.d424_software_engineering_capstone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.readingrecollections.d424_software_engineering_capstone.ui.theme.D424softwareengineeringcapstoneTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            D424softwareengineeringcapstoneTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    D424softwareengineeringcapstoneTheme {
+        Greeting("Android")
+    }
+}
