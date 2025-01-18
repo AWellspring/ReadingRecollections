5ad641d1b09fcd57772f4317202aee4c57dd518d
ncover7 <ncover7@wgu.edu>
2025-01-17 18:56:13 -0600

    Had to revert to the previous project commit, files would not function without the Kotlin dependencies.  Created a WelcomeActivity page.  Uploaded a drawable sourced from Pixabay for a banner image.  Created a button.  Working on styling.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.readingrecollections.d424_software_engineering_capstone"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.readingrecollections.d424_software_engineering_capstone"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -49,6 +49,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
