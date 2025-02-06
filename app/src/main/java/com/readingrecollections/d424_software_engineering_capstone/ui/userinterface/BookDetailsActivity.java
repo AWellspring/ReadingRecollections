583a09b9feaa927e4a13d3717c4411728a8cbf2f
ncover7 <ncover7@wgu.edu>
2025-02-05 18:23:59 -0600

    BookDetailsActivity.java: Call the TextViews by their id. Retrieve book from Repository. Retrieve book variables from the book. If the values exist, they are passed to the text views.

--- app/src/main/java/com/readingrecollections/d424_software_engineering_capstone/ui/userinterface/BookDetailsActivity.java
+++ app/src/main/java/com/readingrecollections/d424_software_engineering_capstone/ui/userinterface/BookDetailsActivity.java
@@ -10,12 +10,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.readingrecollections.d424_software_engineering_capstone.R;
+import com.readingrecollections.d424_software_engineering_capstone.ui.database.DateConverter;
 import com.readingrecollections.d424_software_engineering_capstone.ui.database.Repository;
 import com.readingrecollections.d424_software_engineering_capstone.ui.entities.Book;
+
+import java.time.format.DateTimeFormatter;
 
 public class BookDetailsActivity extends AppCompatActivity {
 
     private TextView bookTitleView;
+
+    private TextView authorNameView;
+
+    private TextView bookGenre;
+
+    private TextView dateRead;
+
+    private TextView isSeries;
+
+    private TextView seriesName;
+
+    private TextView seriesNumber;
 
     private String bookTitle;
 
@@ -25,6 +40,8 @@ public class BookDetailsActivity extends AppCompatActivity {
     private Repository mRepository;
 
     private Book book;
+
+    private DateConverter dateConverter = new DateConverter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +59,12 @@ public class BookDetailsActivity extends AppCompatActivity {
 
         // Finds TextView by id
         bookTitleView = findViewById(R.id.text_book_title);
+        authorNameView = findViewById(R.id.author_name);
+        bookGenre = findViewById(R.id.book_genre);
+        dateRead = findViewById(R.id.date_read);
+        isSeries = findViewById(R.id.is_series);
+        seriesName = findViewById(R.id.series_name);
+        seriesNumber = findViewById(R.id.series_number);
 
         // Passes book title from the intent on the previous page
         bookTitle = getIntent().getStringExtra("book_title");
@@ -52,10 +75,31 @@ public class BookDetailsActivity extends AppCompatActivity {
         // Sets the TextView text to bookTitle
         bookTitleView.setText(bookTitle);
 
+        // Sets the TextView text to authorName
+        authorNameView.setText(authorName);
+
         if (bookTitle != null) {
             mRepository.executor.execute(() -> {
                 authorId = mRepository.getAuthorIdByName(authorName);
                 book = mRepository.getBookByTitleAndAuthor(bookTitle, authorId);
+
+                runOnUiThread(() -> {
+                    if (book.getGenre() != null) {
+                        bookGenre.setText(book.getGenre());
+                    }
+                    if (book.getDateRead() != null) {
+                        dateRead.setText(DateConverter.fromLocalDate(book.getDateRead()));
+                    }
+                    if (book.getSeries()) {
+                        isSeries.setText("Yes");
+                    }
+                    if (book.getSeriesName() != null) {
+                        seriesName.setText(book.getSeriesName());
+                    }
+                    if (book.getSeriesNumber() != 0) {
+                        seriesNumber.setText(Integer.toString(book.getSeriesNumber()));
+                    }
+                });
             });
         }
     }
